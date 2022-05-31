4f68608936693749aa7420f1817bd6ca848bc8f0
Ekom Ibiok <ekom.ib@gmail.com>
2022-05-31 17:04:10 +0100

    modal-props-commit

--- components/OptionModal.js
+++ components/OptionModal.js
@@ -3,7 +3,7 @@ import Modal from "react-modal";
 
 const LoginModal = () => {
   return (
-    <Modal contentLabel="Login" isOpen={true}>
+    <Modal contentLabel="Login" isOpen={false}>
       <h3>Create an account or Login</h3>
     </Modal>
   );
