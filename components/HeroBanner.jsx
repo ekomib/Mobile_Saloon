5437766ec2284fe2888daf57918cd6cb5f7cf3c6
Ekom Ibiok <ekom.ib@gmail.com>
2022-06-02 23:47:32 +0100

    test

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -7,6 +7,7 @@ const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
+        <p>Ekom</p>
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
